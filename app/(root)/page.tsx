7bab56df2f7bc3d83a87300edc4143dce9d1a181
Abhilasha Purwar <abhilasha.purwar@gmail.com>
2024-11-07 17:32:09 +0530

    globals.css & tailwind.config updated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
+import HeaderBox from "@/components/headerbox";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const Home = () => {
+  const loggedIn = { firstName: "Abi" };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">Welcome Adrian</header>
+      </div>
+    </section>
+  );
 };
 
-export default page;
+export default Home;
